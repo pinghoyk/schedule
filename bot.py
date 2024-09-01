9944bc1e8e0441abe1abe2083d2b34555a5c4489
Falbue <64151271+Falbue@users.noreply.github.com>
2024-09-02 00:47:18 +0500

    Вернул всё обратно
    Сорянчик

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ YEAR = 25
 btn_ros23 = InlineKeyboardButton(text="ул. Российская 23", callback_data="ros23") # выбор комплексов колледжа
 btn_blux91 = InlineKeyboardButton(text="ул. Блюхера 91", callback_data="blux91")
 
-x = parser.table_courses("https://pronew.chenk.ru/blocks/manage_groups/website/list.php?id=3") # создание курсов для российской
+x = parser.table_courses() # создание курсов для российской
 buttons = []
 for i in range(len(x)):
     button = InlineKeyboardButton(text=f"{i+1} курс", callback_data=f"select_course_{i+1}")
@@ -164,7 +164,7 @@ def callback_query(call): #обработчик вызовов
 
     if (call.data).split("_")[0] == "select" and (call.data).split("_")[1] == "course":
         if complex == "rus21": 
-            x = parser.table_courses("https://pronew.chenk.ru/blocks/manage_groups/website/list.php?id=3")
+        x = parser.table_courses("https://pronew.chenk.ru/blocks/manage_groups/website/list.php?id=3")
         try:
             groups = (x[f'{(call.data).split("_")[2]} курс'])
             keys = (list(groups.keys()))
