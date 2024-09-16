4a092ed41c655e7c721816f8535f03b15b4dc48e
pinghoyk <pinghoyk009@gmail.com>
2024-09-16 19:25:34 +0500

    кнопка день недеели

--- bot.py
+++ bot.py
@@ -35,6 +35,7 @@ btn_ros23 = InlineKeyboardButton(text="Российская 23", callback_data="
 btn_blux91 = InlineKeyboardButton(text="Блюхера 91", callback_data="blux91")
 btn_return_complex = InlineKeyboardButton(text="Назад", callback_data="return_complex")
 
+btn_day = InlineKeyboardButton(text="День", callback_data="select_day")
 btn_week = InlineKeyboardButton(text="Неделя", callback_data="select_week")
 btn_change_group = InlineKeyboardButton(text="Изменить группу", callback_data="back_courses")
 
